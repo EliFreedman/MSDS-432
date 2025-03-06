54559807382d9b53ce02b0e1ca49670ae0642146
efreedman56 <elibfreedman56@gmail.com>
2025-03-05 19:11:15 -0500

    Added sleep to prevent API overuse

--- src/fetcher-service/cmd/fetcher/main.go
+++ src/fetcher-service/cmd/fetcher/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -25,53 +26,58 @@ func main() {
 		"public_health_statistics":  fmt.Sprintf("https://data.cityofchicago.org/resource/iqnk-2tcu.json?$limit=%d", limit),
 	}
 
-	// Create a channel to receive data from the goroutines
-	dataChan := make(chan map[string]interface{})
-	var wg sync.WaitGroup
+	// Repeat the process 5 times
+	for i := 0; i < 20; i++ {
+		// Create a channel to receive data from the goroutines
+		dataChan := make(chan map[string]interface{})
+		var wg sync.WaitGroup
 
-	// Start a new goroutine for each URL
-	for tableName, url := range urls {
-		wg.Add(1)
-		go func(tableName, url string) {
-			defer wg.Done()
-			ch := make(chan map[string]interface{})
-			go fetch.FetchData(url, ch)
+		// Start a new goroutine for each URL
+		for tableName, url := range urls {
+			wg.Add(1)
+			go func(tableName, url string) {
+				defer wg.Done()
+				ch := make(chan map[string]interface{})
+				go fetch.FetchData(url, ch)
 
-			data := <-ch
-			data["table_name"] = tableName // Add table name to the data
-			dataChan <- data
-		}(tableName, url)
+				data := <-ch
+				data["table_name"] = tableName // Add table name to the data
+				dataChan <- data
+			}(tableName, url)
+		}
+
+		// Collect data from the dataChan and publish to RabbitMQ
+		go func() {
+			for data := range dataChan {
+				log.Printf("Received data for table %s: %v", data["table_name"], data["data"])
+
+				// Convert data to JSON
+				message, err := json.Marshal(data)
+				if err != nil {
+					log.Printf("Failed to marshal data: %v", err)
+					continue
+				}
+
+				queue_name := data["table_name"].(string) + "_raw"
+				log.Printf("Publishing data to queue: %s", queue_name)
+
+				// Publish data to RabbitMQ
+				err = queue.PublishToQueue(queue_name, message)
+				if err != nil {
+					log.Printf("Failed to publish data to queue: %v", err)
+				}
+			}
+		}()
+
+		// Wait for all goroutines to complete
+		go func() {
+			wg.Wait()
+			close(dataChan)
+		}()
+
+		// Wait before repeating the process
+		time.Sleep(time.Second * 120)
 	}
 
-	// Collect data from the dataChan and publish to RabbitMQ
-	go func() {
-		for data := range dataChan {
-			log.Printf("Received data for table %s: %v", data["table_name"], data["data"])
-
-			// Convert data to JSON
-			message, err := json.Marshal(data)
-			if err != nil {
-				log.Printf("Failed to marshal data: %v", err)
-				continue
-			}
-
-			queue_name := data["table_name"].(string) + "_raw"
-			log.Printf("Publishing data to queue: %s", queue_name)
-
-			// Publish data to RabbitMQ
-			err = queue.PublishToQueue(queue_name, message)
-			if err != nil {
-				log.Printf("Failed to publish data to queue: %v", err)
-			}
-		}
-	}()
-
-	// Wait for all goroutines to complete
-	go func() {
-		wg.Wait()
-		close(dataChan)
-	}()
-
-	// Prevent the main function from exiting immediately
 	select {}
 }
