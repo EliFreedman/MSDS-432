d61e08b31d00bf31caeec09f034aab4775f2cfd9
efreedman56 <elibfreedman56@gmail.com>
2025-03-09 13:46:32 -0400

    Began completing requirements

---
+++ analysis/requirement_1.sql
@@ -0,0 +1,10 @@
+-- SELECT week_start, zip_code, SUM(cases_weekly)
+-- FROM public.covid_cases
+-- GROUP BY week_start, zip_code
+-- ORDER BY sum DESC;
+
+-- SELECT *
+-- FROM census_data;
+
+SELECT *
+FROM taxi_trips;
